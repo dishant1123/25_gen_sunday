01f82c6bd93ba4b93dcfe2e1bfcb68b487e57637
dishant1123 <dishantshah1417@gmail.com>
2025-11-16 11:21:42 +0530

    .....

--- 16nov.py
+++ 16nov.py
@@ -163,3 +163,41 @@ menu driven  :
 
 choice : 
 """
+# bank  :
+"""
+Royal Kids Bank :
+
+Design a Banking App in c  that has the following functionalities:-
+User can:-
+◆OPEN ACCOUNT by username and password of his choice. On Opening account, his initial balance will be ₹ 25,000/-. 
+Once he opens account, he can login by re-entering the same username & password.
+◆LOGIN is compulsory to perform any task such as withdrawal, deposit or balance check. If the user name or password do 
+not match, he can not Login. Once he is logged in, he can do as many transactions as he wants. He needs to Logout after 
+he finishes all the transactions
+◆DEPOSIT will enable user to deposit amount of money of his choice. His balance should be updated after the task 
+completes.
+◆WITHDRAW will enable user to withdraw amount of money of his choice. The only condition is that his balance at any 
+point can not go less than ₹10,000/-. If this can happen after his withdrawal, your program must not allow that
+transaction. His balance should be updated after the task completes.
+◆CHECK BALANCE will show the latest updated balance to user.
+◆LOGOUT will exit the user from the program
+You should use these functions in your program: login(), deposit(), withdraw(), checkBalance()
+
+"""
+
+# hint  : 
+"""
+
+creat  username , password : us = d123 ,pass =d@123 
+1. login : us pass : match   :   ac =25000 
+    1. deposit  
+    2. withdraw  :condition  : min  10000   ===> bal =25000 with  ==>19000  ==> no
+        con  : balance - withdraw >=10000 :
+            balance = balance -with_amt
+    else :
+        print("insufficient balance , and  you have required min 10000 rs in your account")
+    3.check balance 
+2.exit 
+
+"""
+
