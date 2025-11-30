27f3530239405c1f4685971a9993e8957dba0b7b
dishant1123 <dishantshah1417@gmail.com>
2025-11-30 10:41:57 +0530

    ....

---
+++ 30nov.py
@@ -0,0 +1,64 @@
+# module  : random  , date time , time ,calendar, math  ,tkinter 
+
+# math : 
+
+import math as m  
+
+"""print(m.sqrt(2))
+print(m.factorial(5))
+print(m.pi)
+print(m.fsum([1,2,3,4,5,6,7,8]))
+
+print(m.floor(2.67))
+print(m.ceil(2.23))
+print(m.e)
+print(m.pow(2,3))
+print(m.remainder(10,3))
+"""
+
+# random  : 
+
+import random as r 
+
+# print(r.random())  # 0-1 float  value
+# print(r.randrange(1,10,2))  # last value  is excluded that is 10 
+
+# print(r.randint(1,5))  # both values are included
+
+# print(r.choice([1,2,3,4,5,6,"chahat","dev",100]))
+# print(r.choices([1,2,3,4,5,6,"chahat","dev",100],k=3))
+
+"""l1=[1,2,3,4,"chahat","dev",100]
+print(l1)
+r.shuffle(l1)
+print(l1)
+"""
+
+# game : 
+
+"""
+1. rock  paper scissor  : 
+
+user  vs computer 
+ 
+==> randint (1,3)  1. rock  2 . paper  3 .scissor / randrange(1,4)
+
+logic : 
+user_score =0 
+computer_score =0
+if user ==1  or com ==1  : 
+    print("tie)
+
+elif user ==2 or com ==1  or user ==1 com ==3  user ==3 com ==2  
+    print("user win )
+    user_score +=1
+else :
+    print("computer win)
+    computer_score +=1
+    
+10  : loop  while  /  for  
+display  : 
+    userscore = 9  computerscore = 1
+    user win 
+function / normal  
+"""
