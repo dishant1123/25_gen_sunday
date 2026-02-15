389bcc3f80496f50da8a33a8610890ab1759b56b
dishant1123 <dishantshah1417@gmail.com>
2026-02-15 10:31:15 +0530

    .....

--- numpy/15feb.py
+++ numpy/15feb.py
@@ -59,7 +59,7 @@ print(a.size)
 """
 
 # slicing  in  2d array  : 
-
+"""
 a=np.array([
     [1,2,3,4,5],
     [6,7,8,9,10],
@@ -75,6 +75,8 @@ print(a[1:4,1:3])  # 1:4 row slice ,  1:3  col slice
 print(a[:,1:5])
 print(a[1:5, : :-1])
 
+print(a[[0,1,2,3,4],[0,1,2,3,4]])
+"""
 # task : 
 """
 1. print  corners : 
@@ -89,3 +91,99 @@ print(a[1:5, : :-1])
             [19,20],
             [24,25]]
 """
+
+# reshape , arange ,size : 
+
+"""a=np.arange(1,20)
+print(a)
+
+b= np.arange(1,21).reshape(5,4)
+print(b)
+
+c=np.arange(1,33).reshape(2,2,2,4)
+print(c)
+
+import  random 
+
+d= np.random.random(size=(3,4))  # 0-1 value 
+d=np.random.random(size =12).reshape(3,4)  # 0-1 value
+print(d)
+
+e=np.random.randint(low=-10,high=10,size=12).reshape(4,3)
+print(e)
+
+f=np.random.sample(size=12).reshape(4,3)
+print(f)
+"""
+
+# flatten  vs ravel : convert 2d array  in to  1d array  
+
+"""
+a=np.arange(1,10).reshape(3,3)
+# print(a)
+a1= a.flatten()  # create new  memory 
+a[2,2] =99
+print("original  matrix  :\n",a)
+print(a1)
+"""
+
+# ravel : 
+"""a=np.arange(1,10).reshape(3,3)
+# print(a)
+a1= a.ravel()   # it  replace the  memory  
+a[2,2] =99
+print("original  matrix  :\n",a)
+print(a1)
+"""
+
+# np.one,np.zeros,full,full_like : 
+
+"""a1=np.ones((3,3),dtype=int)
+print(a1)
+
+b1=np.zeros((3,3),dtype=int)
+print(b1)   
+
+c1=np.full_like(a1,fill_value=99)
+print(c1)
+
+d1=np.full((3,3),fill_value=99)
+print(d1)
+"""
+
+# tranpose ,eye  ==> identity: 
+"""
+a=np.array([[1,2,3],[4,5,6],[7,8,9]],dtype=int)
+print("original  matrix  :\n",a)
+
+print("transpose : \n",np.transpose(a))
+print(a.T)
+
+b1 =np.eye(3,3)
+print(b1)
+"""
+
+# statistical methods : 
+
+"""a=np.array([1,2,3,4,5,6,7,8,9],dtype=int)
+
+print(a)
+print(np.mean(a))
+print(np.median(a))
+print(np.std(a))
+print(np.var(a))
+print(np.sum(a))
+"""
+
+# sum of matrix  : 
+"""
+axis =0   ==> means   col wise sum 
+axis =1   ==> means   row wise sum
+"""
+
+a=np.array([[1,2,3],[4,5,6],[7,8,9]],dtype=int)
+
+print(a)
+print(np.sum(a))
+print(np.sum(a,axis=0))
+print(np.sum(a,axis=1))
